65adf79ef079d1857bfc872b747b1636a2eb2b9f
Ace <waynn7788@gmail.com>
2018-05-21 14:35:13 +0800

    user can leave message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  def index
+    @user = User.new
+    @users = User.all
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to users_url
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :message)
+  end
 end
