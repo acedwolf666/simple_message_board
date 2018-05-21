6aa71ee6234b954578989ea69568572485d81b17
Ace <waynn7788@gmail.com>
2018-05-21 15:31:45 +0800

    add validations / seperate new method from index method in case of eorror occured due to 'each' method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,17 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(user_params)
-    @user.save
-    redirect_to users_url
+    if @user.save
+      redirect_to users_url
+    else
+      render :action => :new
+    end
   end
 
 private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates_presence_of :name, :message
+  validates_length_of :name, :maximum => 20
+  validates_length_of :message, :minimum => 20
 end
